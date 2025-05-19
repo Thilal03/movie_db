7ad19a2d2a9d2025c61987010ea2a869feab8030
ThilalSaidZaidan <thilalzaidan@gmail.com>
2025-05-19 14:36:28 +0700

    seeding dan factory

--- database/factories/MovieFactory.php
+++ database/factories/MovieFactory.php
@@ -3,9 +3,11 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+use App\Models\Category;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\movie>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Movie>
  */
 class MovieFactory extends Factory
 {
@@ -16,8 +18,19 @@ class MovieFactory extends Factory
      */
     public function definition(): array
     {
+        $title = fake()->sentence(rand(3, 6));
+        $slug = Str::slug($title);
+
         return [
-            //
+            'title' => $title,
+            'slug' => $slug,
+            'synopsis' => fake()->paragraphs(rand(3, 5), true),
+            'category_id' => Category::inRandomOrder()->value('id'), // hanya ambil id
+            'year' => fake()->year(),
+            'actors' => fake()->name() . ', ' . fake()->name() . ', ' . fake()->name(),
+            'cover_image' => 'https://picsum.photos/seed/' . Str::random(10) . '/480/640',
+            'created_at' => now(),
+            'updated_at' => now(),
         ];
     }
 }
